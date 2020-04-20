51a95017f7d05a9ed7b05bc77771ca352bb04908
Jens Christensen <paviln@outlook.dk>
2020-04-20 21:36:34 +0200

    Updated database schema

--- schema.sql
+++ schema.sql
@@ -10,9 +10,44 @@ CREATE DATABASE Canteen;
 GO
 use Canteen;
 
-CREATE TABLE product
+CREATE TABLE tblSupplier
 (
-	id int IDENTITY(1,1) PRIMARY KEY,
-	name NCHAR(255) NOT NULL,
-	category int NOT NULL,
+    fldSupplierId int IDENTITY(1,1) PRIMARY KEY,
+    fldName VARCHAR(50) NOT NULL,
+    fldPhone VARCHAR(50) NOT NULL,
+    fldDeliveryTime TIME(7) NOT NULL
+)
+
+CREATE TABLE tblProduct
+(
+	fldProductId int IDENTITY(1,1) PRIMARY KEY,
+	fldName VARCHAR(50) NOT NULL,
+	fldCategory int NOT NULL,
+	fldSupplierId INT FOREIGN KEY REFERENCES tblSupplier(fldSupplierId)
+)
+
+CREATE TABLE tblStaff
+(
+    fldStaffId INT IDENTITY(1,1) PRIMARY KEY,
+    fldName VARCHAR NOT NULL
+)
+
+CREATE TABLE tblEmployee
+(
+   fldEmployeeId INT IDENTITY(1,1) PRIMARY KEY,
+   fldName VARCHAR NOT NULL
+)
+
+CREATE TABLE tblTransaction
+(
+    fldTransactionId int IDENTITY(1,1) PRIMARY KEY,
+    fldOrderDate TIMESTAMP NOT NULL,
+    fldStaffId INT FOREIGN KEY REFERENCES tblStaff(fldStaffId),
+    fldEmployeeId INT FOREIGN KEY REFERENCES tblEmployee(fldEmployeeId)
+)
+
+CREATE TABLE tblTransactionProduct
+(
+    fldTransactionId INT FOREIGN KEY REFERENCES tblTransaction(fldTransactionId),
+    fldProductId INT FOREIGN KEY REFERENCES tblProduct(fldProductId)
 )
