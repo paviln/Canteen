4d605d4a7e10a84dee11e6437bf8600723ba5cd9
rairam303 <ramx0042@easv365.dk>
2020-04-20 09:42:13 +0200

    update

---
+++ src/Employee.java
@@ -0,0 +1,6 @@
+import javafx.event.ActionEvent;
+
+public class Employee {
+    public void handleButton(ActionEvent actionEvent) {
+    }
+}
---
+++ src/Transaction.java
@@ -0,0 +1,6 @@
+import javafx.event.ActionEvent;
+
+public class Transaction {
+    public void handleButton(ActionEvent actionEvent) {
+    }
+}
