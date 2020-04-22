5cd90dc580fc35858426e7417fbd3c8c404c934a
Jens Christensen <paviln@outlook.dk>
2020-04-22 16:57:43 +0200

    Added manager controller and view

--- src/presentation/controllers/ManagerController.java
+++ src/presentation/controllers/ManagerController.java
@@ -1,12 +1,30 @@
 package presentation.controllers;
 
 import javafx.fxml.FXML;
+import persistence.ProductDao;
+import presentation.models.Product;
+
+import java.util.List;
 
 public class ManagerController
 {
     @FXML
     private void showProducts()
     {
+        // Test DAO methods
+        ProductDao productDao = new ProductDao();
 
+        Product product = productDao.get(3);
+
+        System.out.println(product.getName());
+
+        List<Product> products = productDao.getAll();
+
+        for (Product p : products)
+        {
+            System.out.println(p.getName());
+        }
+
+        //productDao.save(new Product("test", "1", 1,1));
     }
 }
